d827086e84c4f7adf15201200601a7450d7fe96a
Connor Williams <Cajwilliams13@gmail.com>
2018-10-16 10:26:34 +1100

    testing

--- commands/user-info.js
+++ commands/user-info.js
@@ -3,6 +3,8 @@ module.exports = {
     description: 'Full Info about the User',
     guildOnly: true,
     execute(message, args) {
+        if (!message.mentions.users.size) {
+
         message.channel.send(`
 **Your Username:** ${message.author.username}
 **Your ID:** ${message.author.id}
@@ -14,5 +16,19 @@ module.exports = {
 **Your Avatar:** ${message.author.displayAvatarURL}
 
         **Courtesy of Maeve Bot**`);
+        }
+        const user = message.mentions.users.first();
+        if (user) {
+            message.channel.send(`
+**Your Username:** ${message.user.username}
+**Your ID:** ${message.user.id}
+**Account Created At:** ${message.user.createdAt}
+**Your Nickname:** ${message.member.nickname}
+**Time Joined:** ${message.member.joinedAt}
+**Highest Role:** ${message.member.highestRole}
+**Role Colour:** ${message.member.displayHexColor}
+**Your Avatar:** ${message.user.displayAvatarURL}
+            `)
+        }
     },
 };
