5963431c2a572798e5f9d76cd15958794b7d3704
Connor Williams <Cajwilliams13@gmail.com>
2018-10-15 11:46:03 +1100

    fixed?

--- commands/nekos.js
+++ commands/nekos.js
@@ -2,9 +2,6 @@ module.exports = {
     name: "nekos",
     description: "use for an image of a cute neko!",
     execute(message) {
-        if (message.mentions.users.size) {
-        const attachment = new Attachment('https://nekos.life');
-        message.channel.send(attachment);
+        message.reply("https://nekos.life");
         }
-    }
 };
