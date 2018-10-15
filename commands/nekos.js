73810ea9b199ba5e3acb38f8d86750d4f43cb82c
Connor Williams <Cajwilliams13@gmail.com>
2018-10-15 12:48:58 +1100

    Nekos fixed

--- commands/nekos.js
+++ commands/nekos.js
@@ -2,15 +2,13 @@ module.exports = {
     name: "nekos",
     description: "use for an image of a cute neko!",
     execute(message) {
-        if(message.mentions.members.size == 1) {
+        if(message.mentions.members.size) {
          let member = message.mentions.members.first()
         const rando_imgs = [
-            'https://media.giphy.com/media/CZpro4AZHs436/giphy.gif',
-            'https://media.giphy.com/media/CZpro4AZHs436/giphy2.gif',
-            'https://media.giphy.com/media/CZpro4AZHs436/giphy3.gif',
+            'https://cdn.nekos.life/neko/neko_007.jpg', 
             ]
             message.channel.send(`${message.author} Have a neko ${member}. Nyaa!`, {
-                file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+            file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
             });
         }
     }
