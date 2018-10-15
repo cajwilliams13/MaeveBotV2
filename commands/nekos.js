ca2556225ff84ae909a38ce05775c67fa13d7345
Connor Williams <Cajwilliams13@gmail.com>
2018-10-15 11:57:02 +1100

    test

--- commands/nekos.js
+++ commands/nekos.js
@@ -2,6 +2,8 @@ module.exports = {
     name: "nekos",
     description: "use for an image of a cute neko!",
     execute(message) {
+        if(message.mentions.members.size == 1) {
+         let member = message.mentions.members.first()
         const rando_imgs = [
             'https://media.giphy.com/media/CZpro4AZHs436/giphy.gif',
             'https://media.giphy.com/media/CZpro4AZHs436/giphy2.gif',
