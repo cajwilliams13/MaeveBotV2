78d09a0a33215ac68d333369ee3e3b33ccb54d7c
Connor Williams <Cajwilliams13@gmail.com>
2018-10-15 11:37:31 +1100

    bug fixing for nekos

--- commands/nekos.js
+++ commands/nekos.js
@@ -2,7 +2,9 @@ module.exports = {
     name: "nekos",
     description: "use for an image of a cute neko!",
     execute(message) {
+        if (message.content === "nekos") {
         const attachment = new Attachment('https://nekos.life');
         message.channel.send(attachment);
+        }
     }
 };
