cd7db2ce72390bd3a7da79483b4e033efbaeb379
Connor Williams <Cajwilliams13@gmail.com>
2018-10-15 11:43:27 +1100

    kk

--- commands/nekos.js
+++ commands/nekos.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "nekos",
     description: "use for an image of a cute neko!",
     execute(message) {
-        if (message.content === "nekos") {
+        if (message.mentions.users.size) {
         const attachment = new Attachment('https://nekos.life');
         message.channel.send(attachment);
         }
