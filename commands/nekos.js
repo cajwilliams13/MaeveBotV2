9684e12d6addaab5d96038c1e0924257ba08e1ac
Connor Williams <Cajwilliams13@gmail.com>
2018-10-15 11:55:25 +1100

    nyehko testing

--- commands/nekos.js
+++ commands/nekos.js
@@ -2,6 +2,13 @@ module.exports = {
     name: "nekos",
     description: "use for an image of a cute neko!",
     execute(message) {
-        message.reply("https://nekos.life");
+        const rando_imgs = [
+            'https://media.giphy.com/media/CZpro4AZHs436/giphy.gif',
+            'https://media.giphy.com/media/CZpro4AZHs436/giphy2.gif',
+            'https://media.giphy.com/media/CZpro4AZHs436/giphy3.gif',
+            ]
+            message.channel.send(`${message.author} Have a neko ${member}. Nyaa!`, {
+                file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+            });
         }
 };
