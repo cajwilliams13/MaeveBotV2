0b95bb7bd098dbb84aaa7ed00e50b44fa03e3bf9
Connor Williams <Cajwilliams13@gmail.com>
2018-10-16 11:14:06 +1100

    added nekos Nya

--- commands/nekos.js
+++ commands/nekos.js
@@ -7,7 +7,21 @@ module.exports = {
         const rando_imgs = [
             'https://cdn.nekos.life/neko/neko_007.jpg',
             './images/ouchy.png',
-            './images/caj_likes_neko.png', 
+            './images/caj_likes_neko.png',
+            'https://cdn.nekos.life/neko/neko249.jpg',
+            'https://cdn.nekos.life/neko/neko_117.png',
+            'https://cdn.nekos.life/neko/neko183.jpg',
+            'https://cdn.nekos.life/neko/neko_183.jpg',
+            'https://cdn.nekos.life/neko/neko119.png',
+            'https://cdn.nekos.life/neko/neko370.jpeg',
+            'https://cdn.nekos.life/neko/neko187.jpg',
+            'https://cdn.nekos.life/neko/neko_408.jpg',
+            'https://cdn.nekos.life/neko/neko342.jpeg',
+            'https://cdn.nekos.life/neko/neko253.jpg',
+            'https://cdn.nekos.life/neko/neko330.jpeg',
+            'https://cdn.nekos.life/neko/neko_010.png',
+            'https://cdn.nekos.life/neko/neko009.png',
+            'https://cdn.nekos.life/neko/neko_393.png',
             ]
             message.channel.send(`Have a neko ${member}. Nyaa!`, {
             file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
@@ -19,6 +33,20 @@ module.exports = {
                 'https://cdn.nekos.life/neko/neko_007.jpg', 
                 './images/ouchy.png',
                 './images/caj_likes_neko.png',
+                'https://cdn.nekos.life/neko/neko249.jpg',
+                'https://cdn.nekos.life/neko/neko_117.png',
+                'https://cdn.nekos.life/neko/neko183.jpg',
+                'https://cdn.nekos.life/neko/neko_183.jpg',
+                'https://cdn.nekos.life/neko/neko119.png',
+                'https://cdn.nekos.life/neko/neko370.jpeg',
+                'https://cdn.nekos.life/neko/neko187.jpg',
+                'https://cdn.nekos.life/neko/neko_408.jpg',
+                'https://cdn.nekos.life/neko/neko342.jpeg',
+                'https://cdn.nekos.life/neko/neko253.jpg',
+                'https://cdn.nekos.life/neko/neko330.jpeg',
+                'https://cdn.nekos.life/neko/neko_010.png',
+                'https://cdn.nekos.life/neko/neko009.png',
+                'https://cdn.nekos.life/neko/neko_393.png',
                 ]
                 message.channel.send(`Have a neko ${member}. Nyaa!`, {
                 file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
