e739f14658c0557fd3debac4ba54b48e6bf8690b
Connor Williams <Cajwilliams13@gmail.com>
2018-10-20 13:10:51 +1100

    added a reaction to #maeve

--- commands/maeve.js
+++ commands/maeve.js
@@ -16,11 +16,13 @@ module.exports = {
                ]
                message.channel.send(`Here is some Maeve ${member}. <:MaeveRaeve:499130281920299009>`, {
                file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+               },
+               message.channel.send('Debugging info: '+JSON.stringify(rando_imgs))
                .then(function (message) {
                 message.react("<:MaeveRaeve:499130281920299009>`,")
                 message.react("<:hug:478531781751209984>")
             })
-               });
+               );
            }
         if(message.mentions.members.size == 0) {
                let member = message.author
@@ -36,11 +38,13 @@ module.exports = {
                    ]
                    message.channel.send(`Here is some Maeve ${member}. <:MaeveRaeve:499130281920299009>`, {
                     file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
-                    .then(function (message) {
-                        message.react("<:MaeveRaeve:499130281920299009>`,")
-                        message.react("<:hug:478531781751209984>")
-                    })
-               }); 
+                   }) .then(function (message) {
+                        message.react(":MaeveRaeve:499130281920299009"); 
+                    }).catch(function(e) {
+                        console.log(e), {
+                        //Something
+                       }
+                        });
+                    } 
        }
-    }
- };
+};
