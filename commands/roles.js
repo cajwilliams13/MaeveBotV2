2b1ce2a4faa9e742937a3ef35c3829f6a8ab7f6f
23019883 <Cajwilliams13@gmail.com>
2019-03-17 19:12:17 +1100

    edited things

---
+++ commands/roles.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "roles",
+    description: "use to create a message with reactions to assign roles",
+    aliases: "roleassign, assign",
+    execute(message){
+        
+    }
+
+}
